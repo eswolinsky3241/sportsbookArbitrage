d151e551b7fdc02b429ed840430afa0035d8a03c
ethan <eswolinsky@gmail.com>
2021-03-20 15:17:06 -0400

    Added logic for arbitrage matchups

---
+++ arbitrage.py
@@ -0,0 +1,38 @@
+from matchup import Book
+
+
+class Arb:
+    def __init__(self, bookA, bookB):
+        self.bookA = bookA
+        self.bookB = bookB
+        self.matchupPairs = []
+        self.partialPairs = []
+
+    def pairMatchups(self):
+        matchupPairs = []
+        partialPairs = []
+
+        for a in self.bookA.matchups:
+            for b in self.bookB.matchups:
+                if (a.teamA == b.teamA and a.teamB == b.teamB) or (a.teamA == b.teamB and a.teamB == b.teamA):
+                    matchupPairs.append((a, b))
+                #only one team matches
+                elif (a.teamA == b.teamA) or (a.teamB == b.teamB) or (a.teamA == b.teamB) or (a.teamB == b.teamA):
+                    partialPairs.append((a, b))
+
+        self.matchupPairs = matchupPairs
+        self.partialPairs = partialPairs
+
+        # If a partial match is found, confirm with user whether they are the same game
+        if len(partialPairs) >= 1:
+            print(str(len(partialPairs)), ' partial matchup(s) found:\n')
+            for i in partialPairs:
+                print('Matchup1:\n ')
+                print(i[0].teamA, ' vs. ', i[0].teamB)
+                print('Matchup2:\n')
+                print(i[1].teamA, ' vs. ', i[1].teamB)
+                print('Are these games the same?(y/n)')
+                user_input = input()
+                if user_input.upper() == 'Y':
+                    self.matchupPairs.append(i)
+
--- main.py
+++ main.py
@@ -3,14 +3,18 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from matchup import Book, Matchup
+from arbitrage import Arb
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
+    # create sportsbook objects
+    fd = Book('https://sportsbook.fanduel.com/sports/navigation/11086.3/11087.3')
+    dk = Book('https://sportsbook.draftkings.com/leagues/basketball/3230960?category=game-lines&subcategory=game')
+    # get Matchups
+    fd.getMatchups()
+    dk.getMatchups()
+    arb = Arb(fd, dk)
+    arb.pairMatchups()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
